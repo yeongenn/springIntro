c820df0566db1c1577a86a79401a80a8cc7ade29
yeongenn <yngn0012@naver.com>
2024-07-03 17:52:15 +0900

    240703 문제풀이 주석 추가

--- spr3di_ex/src/main/java/pack/controller/MyImpl.java
+++ spr3di_ex/src/main/java/pack/controller/MyImpl.java
@@ -2,16 +2,22 @@ package pack.controller;
 
 import java.util.Scanner;
 
+import pack.model.SangpumImpl;
 import pack.model.SangpumInter;
 
 public class MyImpl implements MyInter {
-	private SangpumInter inter;
+	private SangpumInter inter; // SangpumInter를 구현한 여러 클래스를 다 쓰고 싶다
+	// private SangpumImpl sangpumImpl; // 이렇게도 가능은 하지만 SangpumImpl 객체만 사용할 수 있다
 	private String[] result;
 	
-	// 
-	public MyImpl(SangpumInter inter) {
+	// DI - constructor injection
+	public MyImpl(SangpumInter inter) { // SangpumInter 구현한 여러 클래스 중 어떤 걸 쓸지는 생성자 통해 들어온다
 		this.inter = inter;
 	}
+	
+	// 스프링은 다형성 덩어리~
+	// 함수형 언어는 다형성을 구현할 수 X
+	// 따라서 파이썬 같은 언어에 클래스가 도입
 	
 	@Override
 	public void inputData() {
