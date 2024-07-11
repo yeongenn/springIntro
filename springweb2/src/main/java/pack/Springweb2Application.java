44f7411d5a8eef4881c60c98527808f2a2718ec8
yeongenn <yngn0012@naver.com>
2024-07-11 17:15:00 +0900

    240711 Spring Boot start

---
+++ springweb2/src/main/java/pack/Springweb2Application.java
@@ -0,0 +1,13 @@
+package pack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springweb2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springweb2Application.class, args);
+	}
+
+}
